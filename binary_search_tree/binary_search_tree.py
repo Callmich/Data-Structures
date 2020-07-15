ff867e6ed0d6fbbe2c7c92880c9a257d1d507fd5
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-15 11:42:40 -0600

    1st commit for Day 3

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,7 +17,22 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # Case 1: value is < self.value
+        if value < self.value
+            # if there is no left child insert value here
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                #repeat the process on left subtree 
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+                
+
+        # Case 2: value is > or =  self.value
 
     # Return True if the tree contains the value
     # False if it does not
